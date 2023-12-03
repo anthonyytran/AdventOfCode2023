f34a6074698e456d50b56b641e82af336d142d1b
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-12-03 17:24:50 +1100

    test

---
+++ Day2/part1.py
@@ -0,0 +1,15 @@
+with open("day1.txt", "r") as file:
+    data = file.read()
+    data = data.split("\n")
+
+total = 0
+
+for line in data:
+    numbers = [int(char) for char in line if char.isdigit()]
+    if len(numbers) == 1:
+        number = int(str(numbers[0]) + str(numbers[0]))
+    else:
+        number = int(str(numbers[0]) + str(numbers[-1]))
+    total += number
+
+print(total)
